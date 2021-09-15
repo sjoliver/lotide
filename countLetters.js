a2983aaf5c5a4babc1ea3cbd837fef195e3f8350
Sophie <sophiejoliver19+1@gmail.com>
2021-09-15 23:28:32 +0000

    fn to count the number of times a certain letter appears in a given sentence

---
+++ countLetters.js
@@ -0,0 +1,28 @@
+// assertEqual function checks if param1 === param2
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`💚💚💚 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`💔💔💔 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  const result = {};
+  for (const letter of sentence) {
+    if (letter !== " ") {
+      if (result[letter]) {
+        result[letter] += 1;
+      } else {
+        result[letter] = 1;
+      }
+    }
+  }
+  return result;
+};
+
+// TEST CODE
+const sentence = "LHL is cool";
+assertEqual(countLetters(sentence).L, 2);
+
+
