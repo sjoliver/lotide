0937ac33a68d3289c27add12c7936c5273bbf03d
Sophie <sophiejoliver19+1@gmail.com>
2021-09-16 01:13:52 +0000

    fn that checks if a certain value is in a given object, if yes, it returns the key

---
+++ findKeyByValue.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`💚💚💚 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`💔💔💔 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  let result;
+    for (const key in object) { 
+    if (object[key] === value) {
+      result = key;
+    }
+  }
+  return result;
+};
+
+
+
+// TEST CODE
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama"); // true
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined); // true
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "comedy"); // false
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Final Table"), "reality"); // false
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Final Table"), undefined); // true
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi"); // true
