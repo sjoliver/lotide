fa36337f9c18421d1d10125fbac90911c076c133
Sophie <sophiejoliver19+1@gmail.com>
2021-09-16 00:53:52 +0000

    fn for determining the position of letters in a sentence

--- countLetters.js
+++ countLetters.js
@@ -25,4 +25,16 @@ const countLetters = function(sentence) {
 const sentence = "LHL is cool";
 assertEqual(countLetters(sentence).L, 2);
 
-
+// output should look like
+// { 
+//   l: [0],
+//   i: [1, 11],
+//   g: [2],
+//   h: [3, 5, 15, 18],
+//   t: [4, 14],
+//   o: [6, 19],
+//   u: [7, 20],
+//   s: [8, 21],
+//   e: [9, 16, 22],
+//   n: [12]
+// }
---
+++ letterPositions.js
@@ -0,0 +1,40 @@
+const eqArrays = function(array1, array2) {
+  let result;
+  for (let i = 0; i < array1.length; i++) {
+    for (let i2 = 0; i2 < array2.length; i2++) {
+      if (array1[i] === array2[i2]) {
+        result = true;
+      } else {
+        result = false;
+      }
+    }
+  }
+  return result;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(`💚💚💚 Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`💔💔💔 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+// new function
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i)
+    } else {
+      results[sentence[i]] = [i];
+    }
+  }
+  return results;
+};
+
+// TEST CODE
+assertArraysEqual(letterPositions("hello").h, [0]);
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("hello").l, [2, 3]);
+assertArraysEqual(letterPositions("hello").o, [4]);
