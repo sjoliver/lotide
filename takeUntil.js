bd8003657974d198d49cc83567b944c54e26f838
Sophie <sophiejoliver19+1@gmail.com>
2021-09-17 01:13:30 +0000

    new fn that mimics lodash's takeWhile

---
+++ takeUntil.js
@@ -0,0 +1,52 @@
+const eqArrays = function(array1, array2) {
+  let result;
+  for (let i = 0; i < array1.length; i++) {
+    for (let i2 = 0; i2 < array2.length; i2++) {
+      if (array1[i] === array2[i2]) {
+        result = true;
+      } else {
+        result = false;
+      }
+    }
+  }
+  return result;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(`💚💚💚 Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`💔💔💔 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+// New fn like Lodash's takeWhile
+const takeUntil = function(array, callBack) {
+  let newArray = [];
+  for (const element of array) {
+    if (callBack(element)) {
+      return newArray;
+    }
+    newArray.push(element);
+  }
+};
+
+// Expected INPUT
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+
+// ASSERTIONS
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
+
+
+// Expected OUTPUT
+// [ 1, 2, 5, 7, 2 ]
+// ---
+// [ 'I\'ve', 'been', 'to', 'Hollywood' ]
