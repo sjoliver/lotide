6a485310202a41327327f9203cc30ec2471eea47
Sophie <sophiejoliver19+1@gmail.com>
2021-09-20 22:45:21 +0000

    refactoring tail.js to include new unit test

--- tail.js
+++ tail.js
@@ -1,20 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`💚💚💚 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`💔💔💔 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(array) {
   let newArray = array.slice(1);
   return newArray;
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-console.log(tail(words));
-console.log(tail(['hey']));
-console.log(tail([]));
+module.exports = tail;
