75d3ebef5c7595487f0f901670dc4de427a53c45
Sophie <sophiejoliver19+1@gmail.com>
2021-09-20 22:49:50 +0000

    refactoring eqArrays to include unit testing

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`💚💚💚 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`💔💔💔 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
   let result;
   for (let i = 0; i < array1.length; i++) {
@@ -20,9 +12,4 @@ const eqArrays = function(array1, array2) {
   return result;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-
-
+module.exports = eqArrays;
