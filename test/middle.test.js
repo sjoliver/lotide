b02d2098019b20e3913968daf6a9c7ae4b3121da
Sophie <sophiejoliver19+1@gmail.com>
2021-09-20 23:07:25 +0000

    refactor middle.js to include unit testing

---
+++ test/middle.test.js
@@ -0,0 +1,12 @@
+const assertArraysEqual = require('../assertArraysEqual');
+const middle = require('../middle');
+
+// test code
+const oddArray = [1, 2, 3];
+const evenArray = [1, 2, 3, 4];
+const smArray = [1, 2];
+
+assertArraysEqual(middle(oddArray), [2]); // pass
+assertArraysEqual(middle(evenArray), [2, 3]); // pass
+assertArraysEqual(middle(smArray), []); 
+
