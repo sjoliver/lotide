b3d0091d6161c3994ef2434843a82fd3d4e935b2
Sophie <sophiejoliver19+1@gmail.com>
2021-09-21 22:23:53 +0000

    refactoring tail and middle to use mocha & chai assertions

--- test/middle.test.js
+++ test/middle.test.js
@@ -1,11 +1,15 @@
-const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
+const assert = require('chai').assert;
 
-// test code
-const oddArray = [1, 2, 3];
-const evenArray = [1, 2, 3, 4];
-const smArray = [1, 2];
 
-assertArraysEqual(middle(oddArray), [2]); // pass
-assertArraysEqual(middle(evenArray), [2, 3]); // pass
-assertArraysEqual(middle(smArray), []); // pass
+describe("#middle", () => {
+  it("should return [2] from [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2])
+  })
+  it("should return [2, 3] from [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3])
+  })
+  it("should return empty array from [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), [])
+  })
+})
--- test/tail.test.js
+++ test/tail.test.js
@@ -1,6 +1,8 @@
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  it("returns 2 & 3 from [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  })
+});
